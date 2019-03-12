dfe7fe624b0cc734c11b893917a6cf1a386ea907
ben-williamson-1999 <ben.williamson1@ntlworld.com>
2019-03-12 00:29:54 +0000

    adding mapbox features, no access token in repo

--- app/src/main/java/mmu/stu/ac/coursework/transport_finder_app/MainActivity.java
+++ app/src/main/java/mmu/stu/ac/coursework/transport_finder_app/MainActivity.java
@@ -2,12 +2,22 @@ package mmu.stu.ac.coursework.transport_finder_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(){
+
     }
 }
